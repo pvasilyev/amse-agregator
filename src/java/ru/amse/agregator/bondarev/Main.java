7b536a990978f509819a8db21384bf0982579afa
bondarevts <bondarevts@def51087-30bf-c014-2c78-f9f0ffc3c973>
2010-11-01 23:13:08 +0000

    Обновил классы индекса и поиска для текущих целей

--- src/java/ru/amse/agregator/bondarev/Main.java
+++ src/java/ru/amse/agregator/bondarev/Main.java
@@ -5,11 +5,11 @@ import ru.amse.agregator.searcher.*;
 import ru.amse.agregator.indexer.*;
 import ru.amse.agregator.storage.*;
 
-import javax.xml.crypto.Data;
 import java.awt.geom.Point2D;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 /*
  * Author: Bondarev Timofey
@@ -20,15 +20,14 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         File indexDir = new File("index");
-        //File dataDir = new File("data");
 
-        //writeDataToDB();
-        //DataBase.printAll();
         DataBase.connectToDirtyBase();
+        writeDateToDB();
+        DataBase.printAll();
         try {
             Indexer.makeNewIndex(indexDir);
             Searcher.setIndexDir(indexDir);
-            Searcher.search(new UserQuery("test"));
+            Searcher.search(new UserQuery("Колизей"));
         } catch (IOException e) {
             System.out.println("IOException! Message: " + e.getMessage());
         } catch (ParseException e) {
@@ -40,61 +39,53 @@ public class Main {
         System.out.println("Done!");
     }
 
-    private static void writeDataToDB() {
-        DataBase.connectToDirtyBase();
+    private static void writeDateToDB() {
+        DataBase.removeCollection(DataBase.COLLECTION_MAIN);
+              
+        DBWrapper someCity = new DBWrapper();
 
-        ArchitectualAttraction x1 = new ArchitectualAttraction();
-        x1.setName("korgov attraction");
-        x1.setPhoto("img.jpg");
-        x1.setCityByName("kllandia");
-        x1.setCoordinates(new Point2D.Double(1220.32432, 10.234324));
-        x1.setDescription("Who I am?");
-        x1.setKeyWordsArray(new ArrayList<String>());
-        x1.setAddress("kl address");
-        x1.setArchitect("korgov a.a.");
-        x1.setCost("100 $");
-        //x1.setDate(new Date(1990,87,8));
-        x1.setType("ìóçåé");
+        someCity.setType(DBWrapper.TYPE_CITY);
+        someCity.setName("Рим");
+        someCity.setPhoto("images/rim.jpg");
+        someCity.setCoordinates(new Point2D.Double(1220.32432,10.234324));
+        someCity.setDescription("Описание Рима");
 
-        DataBase.add(x1);
-        NaturalAttraction x2 = new NaturalAttraction();
-        x2.setName("natural attraction");
-        x2.setPhoto("img.jpg");
-        x2.setCityByName("kllandia");
-        x2.setCoordinates(new Point2D.Double(1220.32432, 10.234324));
-        x2.setDescription("Who I am?");
-        x2.setKeyWordsArray(new ArrayList<String>());
-        DataBase.add(x2);
+        ArrayList<String> stringsArray = new ArrayList<String>();
+        stringsArray.add("beautiful");
+        stringsArray.add("city");
+        stringsArray.add("rim");
+        stringsArray.add("Рим");
+        someCity.setKeyWordsArray(stringsArray);
 
-        City x3 = new City();
-        x3.setName("korgov city");
-        x3.setPhoto("imgcity.jpg");
-        x3.setCoordinates(new Point2D.Double(1220.32432, 10.234324));
-        x3.setDescription("Who I am city?");
-        x3.setKeyWordsArray(new ArrayList<String>());
-        DataBase.add(x3);
+        DataBase.add(someCity);
 
+        DBWrapper someArchAttr = new DBWrapper();
+        someArchAttr.setType(DBWrapper.TYPE_ARCH_ATTRACTION);
+        someArchAttr.setName("Колизей");
 
-        Cafe x4 = new Cafe();
-        x4.setName("korgov cafe");
-        x4.setPhoto("imgcitycafe.jpg");
-        x4.setCoordinates(new Point2D.Double(1220.32432, 10.234324));
-        x4.setDescription("Who I am cafe?");
-        x4.setKeyWordsArray(new ArrayList<String>());
-        x4.setCityByName("new new city");
-        x4.setWebsite("www.korgov.ru");
-        DataBase.add(x4);
+        ArrayList<String> photosArray = new ArrayList<String>();
+        photosArray.add("images/kolizey_im1.jpg");
+        photosArray.add("images/kolizey_im2.jpg");
+        photosArray.add("images/kolizey_im3.jpg");
+        photosArray.add("images/kolizey_im4.jpg");
+        someArchAttr.setPhotosArray(photosArray);
+        
+        someArchAttr.setCityById(someCity.getId());
+        someArchAttr.setCoordinates(new Point2D.Double(10.324234,1022.24234));
+        someArchAttr.setDescription("Kolizey description");
 
-        Hotel x6 = new Hotel();
-        x6.setName("korgov hotel");
-        x6.setPhoto("imghotel.jpg");
-        x6.setCoordinates(new Point2D.Double(1220.32432, 10.234324));
-        x6.setDescription("Who I am hotel?");
-        x6.setKeyWordsArray(new ArrayList<String>());
-        x6.setCityByName("super-city");
-        x6.setWebsite("www.korgov.ru");
-        x6.setRooms("super-luks");
-        DataBase.add(x6);
+        ArrayList<String> keyWordsArray = new ArrayList<String>();
+        keyWordsArray.add("rim");
+        keyWordsArray.add("kolizey");
+        keyWordsArray.add("keyword3");
+        someArchAttr.setKeyWordsArray(keyWordsArray);
+
+        someArchAttr.setAddress("Kolizey Street, 55");
+        someArchAttr.setArchitect("Kirill K.");
+        someArchAttr.setCost("100 $");
+        someArchAttr.setDate(new Date(1990,7,8));
+
+        DataBase.add(someArchAttr);
     }
 }
 
