fb9f5f569c82296039a89eb16560ee26df0bf8b8
Natalia.Ukhorskaya <Natalia.Ukhorskaya@def51087-30bf-c014-2c78-f9f0ffc3c973>
2010-11-13 20:36:09 +0000

    gui changed and merged with index

--- src/java/ru/amse/agregator/gui/yalets/AbstractAgregatorYalet.java
+++ src/java/ru/amse/agregator/gui/yalets/AbstractAgregatorYalet.java
@@ -1,12 +1,19 @@
 package ru.amse.agregator.gui.yalets;
 
+import net.sf.xfresh.core.InternalResponse;
 import net.sf.xfresh.core.Yalet;
 import org.springframework.beans.factory.annotation.Required;
+import ru.amse.agregator.gui.model.Attraction;
 import ru.amse.agregator.gui.model.AttractionManager;
 
-public abstract class AbstractAgregatorYalet implements Yalet {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-    protected AttractionManager manager;
+public abstract class AbstractAgregatorYalet implements Yalet {
+    
+
+    protected AttractionManager manager = new AttractionManager();
 
     @Required
     public void setManager(AttractionManager manager) {
