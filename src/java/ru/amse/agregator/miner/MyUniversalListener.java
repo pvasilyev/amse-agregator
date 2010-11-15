76904daf4068ca81d72b4edf38b7b443badce49b
korgovk <korgovk@def51087-30bf-c014-2c78-f9f0ffc3c973>
2010-11-15 02:09:56 +0000

    miner bug fixes

--- src/java/ru/amse/agregator/miner/MyUniversalListener.java
+++ src/java/ru/amse/agregator/miner/MyUniversalListener.java
@@ -160,20 +160,21 @@ public class MyUniversalListener implements ScraperRuntimeListener {
 	//This method clears string from tags
 	private String clearString(String incomed){
 		
-		if(incomed != null){
-			if(incomed.equals(""))
-				return null;
+		if(incomed != null && !incomed.isEmpty()){
 			int a,b;
-			while(incomed.indexOf('<') != (-1)){
+			while(incomed.indexOf('<') != (-1) && incomed.indexOf('>') != (-1)){
 				a = incomed.indexOf('<');
-				b = incomed.indexOf('>');
+				b = incomed.indexOf('>', a);
 				if( b > a ){
-					incomed = incomed.substring(0,a) + incomed.substring(b,incomed.length());
+					incomed = incomed.substring(0,a) + incomed.substring(b);
 					incomed = incomed.replaceFirst(">", " ");
+				}
+				else {
+					break;
 				}
 			}	
 			incomed = incomed.replaceAll("\n", " ");
-			incomed = incomed.replaceAll("\t", "");
+			incomed = incomed.replaceAll("\t", " ");
 			incomed = incomed.replaceAll(" {2,}", " ");
 			incomed = incomed.replaceAll(" {1,}[.]", ".");
 			incomed = incomed.replaceAll(" {1,}[,]", ",");
