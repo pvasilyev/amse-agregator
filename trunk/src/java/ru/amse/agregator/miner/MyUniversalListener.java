0d7f29aadec10acebe762c8f0e2bd0ebaad19ff7
GorbunovaIN <GorbunovaIN@def51087-30bf-c014-2c78-f9f0ffc3c973>
2010-11-21 18:52:15 +0000

    miner fixed

--- trunk/src/java/ru/amse/agregator/miner/MyUniversalListener.java
+++ trunk/src/java/ru/amse/agregator/miner/MyUniversalListener.java
@@ -96,7 +96,9 @@ public class MyUniversalListener implements ScraperRuntimeListener {
 			
 			DBWrapper newEntry = new DBWrapper();
 			for(int i=0; i<myList.size(); i+=2){
-				if(myList.get(i) != null && myList.get(i+1) != null){
+				
+				if(myList.get(i) != null ){
+					
 				
 				    if(myList.get(i).toString().equals(DBWrapper.FIELD_PHOTOS)){
 					
