1479c4c70e0dada98e40fe3b307cca7b1018ab3f
Natalia.Ukhorskaya <Natalia.Ukhorskaya@def51087-30bf-c014-2c78-f9f0ffc3c973>
2010-11-23 13:21:18 +0000

    gui with menu navigation

--- trunk/src/java/ru/amse/agregator/gui/yalets/CityPageYalet.java
+++ trunk/src/java/ru/amse/agregator/gui/yalets/CityPageYalet.java
@@ -2,8 +2,10 @@ package ru.amse.agregator.gui.yalets;
 
 import java.util.ArrayList;
 
+import org.apache.log4j.Logger;
 import org.bson.types.ObjectId;
 
+import ru.amse.agregator.gui.model.Attraction;
 import ru.amse.agregator.gui.model.DescriptionModel;
 import ru.amse.agregator.gui.model.MenuItem;
 import ru.amse.agregator.gui.model.UniversalModel;
@@ -14,30 +16,32 @@ import net.sf.xfresh.core.InternalResponse;
 import net.sf.xfresh.core.Yalet;
 
 public class CityPageYalet implements Yalet{
-
+    Logger log = Logger.getLogger(ShowAttractionsYalet.class);
+    
 	@Override
 	public void process(InternalRequest req, InternalResponse res) {
-	
+
 		Database.connectToDirtyBase();
 		String id = req.getParameter("id");
 		ObjectId selectedCity = new ObjectId(id);
-		
+
 		DBWrapper city = Database.getDBObjectByIdAndType(selectedCity, DBWrapper.TYPE_CITY);
-		
-		DescriptionModel response = new DescriptionModel();
+
+		Attraction response = new Attraction();
 		response.setName(city.getName());
 		response.setDescription(city.getDescriptionArray().get(0));
-		response.setImages(city.getImagesArray());
-		
+		response.setPhotoArray(city.getImagesArray());
+
 		ArrayList<String> list = Database.getAllTypesOfObjectByCity(selectedCity);
-		
+
 		ArrayList<MenuItem> links = new ArrayList<MenuItem>();
-		
+
 		for(String tmp : list){
 			MenuItem newCity = new MenuItem(tmp , id);
 			links.add(newCity);
 		}
-		response.setCities(links);
+        log.error(links);
+		response.setAttactionList(links);
 		res.add(response);
 	}
 }
