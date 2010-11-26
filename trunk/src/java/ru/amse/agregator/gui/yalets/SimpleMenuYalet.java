4d683f79dc642c4d541fa49e43a1aa2af208132b
Natalia.Ukhorskaya <Natalia.Ukhorskaya@def51087-30bf-c014-2c78-f9f0ffc3c973>
2010-11-26 20:13:13 +0000

    gui: удаление лишнего

---
+++ trunk/src/java/ru/amse/agregator/gui/yalets/SimpleMenuYalet.java
@@ -0,0 +1,34 @@
+package ru.amse.agregator.gui.yalets;
+
+import java.util.ArrayList;
+
+import net.sf.xfresh.core.InternalRequest;
+import net.sf.xfresh.core.InternalResponse;
+import net.sf.xfresh.core.Yalet;
+import org.apache.log4j.Logger;
+import ru.amse.agregator.gui.model.AttractionManager;
+import ru.amse.agregator.gui.model.MenuItem;
+import ru.amse.agregator.storage.*;
+
+public class SimpleMenuYalet extends AbstractAgregatorYalet {
+    Logger log = Logger.getLogger(ShowAttractionsYalet.class);
+
+
+    public void process(InternalRequest req, InternalResponse res) {
+
+        AttractionManager.connectToDatabase();
+        ArrayList<DBWrapper> continents = Database.getAllContinents();
+
+        for (DBWrapper continent : continents) {
+
+            ArrayList<DBWrapper> countrytmp = Database.getAllCountriesByContinent(continent.getId());
+
+            ArrayList<MenuItem> menuItems = new ArrayList<MenuItem>();
+            menuItems.add(new MenuItem(continent.getName(), null));
+            for (DBWrapper country : countrytmp) {
+                menuItems.add(new MenuItem(country.getName(), country.getId().toString()));
+            }
+            res.add(menuItems);
+        }
+    }
+}
