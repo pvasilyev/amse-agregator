20ee0ec88b00e360f0e9330d9a3569605d335078
vasiliev.paul@gmail.com <vasiliev.paul@gmail.com@def51087-30bf-c014-2c78-f9f0ffc3c973>
2011-04-03 18:09:46 +0000

    try to do smth with ranking yalet

---
+++ trunk/src/java/ru/amse/agregator/gui/yalets/RankingBlockYalet.java
@@ -0,0 +1,18 @@
+package ru.amse.agregator.gui.yalets;
+
+import net.sf.xfresh.core.InternalRequest;
+import net.sf.xfresh.core.InternalResponse;
+import net.sf.xfresh.core.Yalet;
+
+/**
+ * User: nepank
+ * Date: 03.04.11
+ * Time: 17:25
+ */
+public class RankingBlockYalet implements Yalet {
+
+    public void process(InternalRequest internalRequest, InternalResponse internalResponse) {
+
+    }
+
+}
