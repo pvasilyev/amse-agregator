b5734c29f253a218d3252aa1f75519c7dcb1f359
talanov.pavel <talanov.pavel@def51087-30bf-c014-2c78-f9f0ffc3c973>
2011-04-23 19:13:32 +0000

    добавил словарь хороших слов в ресурсы

--- trunk/src/java/ru/amse/agregator/quality/clusterization/ClusterizationHelper.java
+++ trunk/src/java/ru/amse/agregator/quality/clusterization/ClusterizationHelper.java
@@ -6,7 +6,10 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import javax.swing.event.ListSelectionEvent;
+import net.sf.saxon.functions.Collection;
 import ru.amse.agregator.storage.Database;
 import ru.amse.agregator.quality.clusterization.clusterstorage.*;
 import ru.amse.agregator.quality.clusterization.simgraph.*;
@@ -19,30 +22,62 @@ import ru.amse.agregator.storage.UniqueId;
 /**
  *
  * @author pavel
+ *
+ * Only some code for testing and creating some temporary files
  */
 public class ClusterizationHelper {
 
     static public void main(String[] args) throws Exception {
-        Database.connectToDirtyBase();
-        String fileName = "myDescriptions.txt";
-        BufferedWriter outputStream = null;
-        try {
-            outputStream =
-                new BufferedWriter(new FileWriter(fileName));
+//        Database.connectToMainBase();
+//        String fileName1 = "/home/pavel/myDescriptions.txt";
+//        String fileName = "/home/pavel/myVocabulary.txt";
+//        FrequencyList list = new FrequencyList();
+//
+//        ArrayList<DBWrapper> cities = Database.getAllWithType(DBWrapper.TYPE_CITY);
+//        for (DBWrapper city : cities) {
+//            List<String> descriptions = city.getDescriptionArray();
+//            if (descriptions != null) {
+//                for (String description : descriptions) {
+//                    list.addText(description);
+//                }
+//            }
+//
+//        }
+//
+//        BufferedWriter outputStream = null;
+//        try {
+//            outputStream =
+//                new BufferedWriter(new FileWriter(fileName));
+//
+//            List<String> sortedVocabulary = list.getSortedVocabularyOfMinimumFrequency(100);
+//
+//            for (String word : sortedVocabulary) {
+//                outputStream.write(word + "\n");
+//            }
+//
+//
+//        } finally {
+//            if (outputStream != null) {
+//                outputStream.close();
+//            }
+//        }
 
-            ArrayList<DBWrapper> cities = Database.getAllWithType(DBWrapper.TYPE_CONTINENT);
-            for (DBWrapper city : cities) {
-                List<String> descriptions = city.getDescriptionArray();
-                for (String description : descriptions) {
-                    outputStream.write(description);
-                }
+        QualityDescriptionMerger qualityMerger = new QualityDescriptionMerger();
+        FingerprintDescriptionMerger fingerprintMerger = new FingerprintDescriptionMerger(new DescriptionFingerprinter());
+        ArrayList<String> desc = new ArrayList<String>();
+        desc.add("прекрасный город прекрасный");
+        desc.add("плохой город прекрасный прекрасный прекрасный");
+        desc.add("троолололол трололо троололо");
+        ArrayList<String> sortedDescriptions
+                = new ArrayList<String>(qualityMerger.sortByQuality(desc));
+        List<String> resultingList = fingerprintMerger.filterWithFingerprinter(sortedDescriptions);
 
-            }
-        } finally {
-            if (outputStream != null) {
-                outputStream.close();
-            }
+        for (String res : resultingList) {
+            System.out.println(res);
         }
+        Collections.reverse(resultingList);
+
+
     }
 
 }
