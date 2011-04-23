67c9778badbfea06efc058321ed92c786d21a138
talanov.pavel <talanov.pavel@def51087-30bf-c014-2c78-f9f0ffc3c973>
2011-04-23 20:08:36 +0000

    Добавил проверку для отсеивания объектов с короткими описаниями или без описаний.

--- src/java/ru/amse/agregator/quality/clusterization/merge/MergeProcess.java
+++ src/java/ru/amse/agregator/quality/clusterization/merge/MergeProcess.java
@@ -1,5 +1,6 @@
 package ru.amse.agregator.quality.clusterization.merge;
 
+import java.util.ArrayList;
 import ru.amse.agregator.quality.clusterization.clusterstorage.Cluster;
 import ru.amse.agregator.quality.clusterization.clusterstorage.ClusterStorage;
 import ru.amse.agregator.storage.DBWrapper;
@@ -12,6 +13,8 @@ import ru.amse.agregator.storage.UniqueId;
  * @author pavel
  */
 final public class MergeProcess {
+
+    static private int MINIMUM_DESCRIPTION_LENGTH = 20;
 
     static public void perform(ClusterMerger merger, ClusterStorage storage) {
 
@@ -31,18 +34,20 @@ final public class MergeProcess {
             }
 
             DBWrapper obj = merger.mergeCluster(cluster);
-            if(obj.getName() != null && !obj.getName().isEmpty()){
-                // add or update an object
-                UniqueId objectToUpdateId = cluster.objectFromMainDB();
-                if (objectToUpdateId != null) {
-                    // update the correct object
-                    Database.connectToMainBase();
-                    obj.setId(objectToUpdateId.getId());
-                    Database.update(obj);
-                } else {
-                    // add a new object
-                    Database.connectToMainBase();
-                    Database.add(obj);
+            if (checkObjectConsistency(obj)) {
+                if(obj.getName() != null && !obj.getName().isEmpty()){
+                    // add or update an object
+                    UniqueId objectToUpdateId = cluster.objectFromMainDB();
+                    if (objectToUpdateId != null) {
+                        // update the correct object
+                        Database.connectToMainBase();
+                        obj.setId(objectToUpdateId.getId());
+                        Database.update(obj);
+                    } else {
+                        // add a new object
+                        Database.connectToMainBase();
+                        Database.add(obj);
+                    }
                 }
             }
             
@@ -51,5 +56,23 @@ final public class MergeProcess {
         storage.finishIterating();
 
         merger.postprocess(storage);
+    }
+
+    // checks if the object is consistent to add to the base
+    static private boolean checkObjectConsistency(DBWrapper object) {
+        ArrayList<String> descriptions = object.getDescriptionArray();
+        if (descriptions == null) {
+            return false;
+        }
+
+        for (String description : descriptions) {
+            if (description.length() > MINIMUM_DESCRIPTION_LENGTH) {
+                return true;
+            }
+        }
+
+        // all descriptions are too short
+        return false;
+
     }
 }
