546be0d93b3e606382566e75c6bdceb5c48109e3
vasiliev.paul@gmail.com <vasiliev.paul@gmail.com@def51087-30bf-c014-2c78-f9f0ffc3c973>
2011-04-26 21:33:13 +0000

    categories testing

--- trunk/src/java/ru/amse/agregator/ranking/CategoriesTest.java
+++ trunk/src/java/ru/amse/agregator/ranking/CategoriesTest.java
@@ -5,6 +5,7 @@ import ru.amse.agregator.storage.DBWrapper;
 import ru.amse.agregator.storage.Database;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * User: nepank
@@ -15,11 +16,24 @@ import java.io.IOException;
 public class CategoriesTest {
     public void test() {
         Database.connectToDirtyBase();
-        DBWrapper redFort = Database.getDBObjectByIdAndType(new ObjectId("4d9835b0bc76b0864869d2b5"), DBWrapper.TYPE_ATTRACTION);
+        DBWrapper redFort = Database.getDBObjectByIdAndType(new ObjectId("4db5f37b2420b0864ee35ec0"), DBWrapper.TYPE_ARCH_ATTRACTION);
         System.out.println(redFort.getKeyWordsArray());
         System.out.println("redFort in ATTRACTION category : " + redFort.isInCategory(Categories.ATTRACTION));
         System.out.println("redFort in BUILDING category : " + redFort.isInCategory(Categories.BUILDING));
         System.out.println("redFort in BRIDGE category : " + redFort.isInCategory(Categories.BRIDGE));
+
+//        final ArrayList<String> tops = Database.getTopNCategoriesInContinent(10, new ObjectId("4db5f3742420b08648e35ec0"));
+//        for (String top : tops) {
+//            System.out.println(top);
+//        }
+
+        final ArrayList<String> tops = Database.getTopNCategories(10);
+        for (String top : tops) {
+            System.out.println(top);
+        }
+//        final ArrayList<DBWrapper> allCities = Database.getAllCitiesByCountry(new ObjectId("4db5f4a72420b08697e45ec0"));
+//        Database.getAllDBObjectsWithKeyValue(DBWrapper.FIELD_COUNTRY_ID, "4db5f4a72420b08697e45ec0");
+//        System.out.println(Database.getAllDBObjectsWithKeyValue(DBWrapper.FIELD_COUNTRY_ID, "4db5f4a72420b08697e45ec0").get(0).getName());
     }
 
     public static void main(String[] args) throws IOException {
